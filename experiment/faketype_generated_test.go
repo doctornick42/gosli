175a25cba3419d325fbe943a9e0c55cf89d8f885
doctornick <dmitry.bogomolov@iage.net>
2019-01-03 00:00:33 +0300

    Cover First, FirstOrDefault, Where, Select methods with unit tests

---
+++ experiment/faketype_generated_test.go
@@ -0,0 +1,266 @@
+package experiment
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type FakeTypeTestSuite struct {
+	suite.Suite
+	mockCtrl *gomock.Controller
+}
+
+func TestFactory(t *testing.T) {
+	suite.Run(t, new(FakeTypeTestSuite))
+}
+
+func (ts *FakeTypeTestSuite) TestFakeTypeFirstOrDefault() {
+	sl := []*FakeType{
+		&FakeType{
+			A: 1,
+			B: "one",
+		},
+		&FakeType{
+			A: 2,
+			B: "two",
+		},
+		&FakeType{
+			A: 3,
+			B: "three",
+		},
+	}
+
+	testCases := []struct {
+		name        string
+		sl          []*FakeType
+		filter      func(*FakeType) bool
+		expectedRes *FakeType
+	}{
+		{
+			name: "found",
+			sl:   sl,
+			filter: func(ft *FakeType) bool {
+				return ft.A == 2
+			},
+			expectedRes: &FakeType{
+				A: 2,
+				B: "two",
+			},
+		},
+		{
+			name: "not found",
+			sl:   sl,
+			filter: func(ft *FakeType) bool {
+				return ft.A == 123
+			},
+			expectedRes: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		ts.initDependencies()
+
+		ts.T().Run(tc.name, func(t *testing.T) {
+			res := FakeTypeFirstOrDefault(tc.sl, tc.filter)
+
+			if tc.expectedRes == nil {
+				assert.Nil(t, res)
+				return
+			}
+
+			isEqualToExpected, err := res.Equal(tc.expectedRes)
+			assert.Nil(t, err)
+			assert.True(t, isEqualToExpected)
+		})
+	}
+}
+
+func (ts *FakeTypeTestSuite) TestFakeTypeFirst() {
+	sl := []*FakeType{
+		&FakeType{
+			A: 1,
+			B: "one",
+		},
+		&FakeType{
+			A: 2,
+			B: "two",
+		},
+		&FakeType{
+			A: 3,
+			B: "three",
+		},
+	}
+
+	testCases := []struct {
+		name        string
+		sl          []*FakeType
+		filter      func(*FakeType) bool
+		expectedRes *FakeType
+		expectedErr error
+	}{
+		{
+			name: "found",
+			sl:   sl,
+			filter: func(ft *FakeType) bool {
+				return ft.A == 2
+			},
+			expectedRes: &FakeType{
+				A: 2,
+				B: "two",
+			},
+		},
+		{
+			name: "not found",
+			sl:   sl,
+			filter: func(ft *FakeType) bool {
+				return ft.A == 123
+			},
+			expectedErr: errors.New("Not found"),
+		},
+	}
+
+	for _, tc := range testCases {
+		ts.initDependencies()
+
+		ts.T().Run(tc.name, func(t *testing.T) {
+			res, err := FakeTypeFirst(tc.sl, tc.filter)
+
+			if tc.expectedErr == nil {
+				isEqualToExpected, err := res.Equal(tc.expectedRes)
+				assert.Nil(t, err)
+				assert.True(t, isEqualToExpected)
+			} else {
+				assert.NotNil(t, err)
+				assert.Equal(t, tc.expectedErr.Error(), err.Error())
+			}
+		})
+	}
+}
+
+func (ts *FakeTypeTestSuite) TestFakeTypeWhere() {
+	sl := []*FakeType{
+		&FakeType{
+			A: 1,
+			B: "one",
+		},
+		&FakeType{
+			A: 2,
+			B: "two",
+		},
+		&FakeType{
+			A: 3,
+			B: "three",
+		},
+	}
+
+	testCases := []struct {
+		name        string
+		sl          []*FakeType
+		filter      func(*FakeType) bool
+		expectedRes []*FakeType
+	}{
+		{
+			name: "found",
+			sl:   sl,
+			filter: func(ft *FakeType) bool {
+				return ft.A > 1
+			},
+			expectedRes: []*FakeType{
+				&FakeType{
+					A: 2,
+					B: "two",
+				},
+				&FakeType{
+					A: 3,
+					B: "three",
+				},
+			},
+		},
+		{
+			name: "not found",
+			sl:   sl,
+			filter: func(ft *FakeType) bool {
+				return ft.A == 123
+			},
+			expectedRes: []*FakeType{},
+		},
+	}
+
+	for _, tc := range testCases {
+		ts.initDependencies()
+
+		ts.T().Run(tc.name, func(t *testing.T) {
+			res := FakeTypeWhere(tc.sl, tc.filter)
+
+			assert.EqualValues(t, tc.expectedRes, res)
+		})
+	}
+}
+
+func (ts *FakeTypeTestSuite) TestFakeTypeSelect() {
+	sl := []*FakeType{
+		&FakeType{
+			A: 1,
+			B: "one",
+		},
+		&FakeType{
+			A: 2,
+			B: "two",
+		},
+		&FakeType{
+			A: 3,
+			B: "three",
+		},
+	}
+
+	type tempType struct {
+		Msg string
+	}
+
+	testCases := []struct {
+		name        string
+		sl          []*FakeType
+		filter      func(*FakeType) interface{}
+		expectedRes []interface{}
+	}{
+		{
+			name: "everything ok",
+			sl:   sl,
+			filter: func(ft *FakeType) interface{} {
+				return &tempType{
+					Msg: fmt.Sprintf("%v-%s", ft.A, ft.B),
+				}
+			},
+			expectedRes: []interface{}{
+				&tempType{
+					Msg: "1-one",
+				},
+				&tempType{
+					Msg: "2-two",
+				},
+				&tempType{
+					Msg: "3-three",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		ts.initDependencies()
+
+		ts.T().Run(tc.name, func(t *testing.T) {
+			res := FakeTypeSelect(tc.sl, tc.filter)
+
+			assert.EqualValues(t, tc.expectedRes, res)
+		})
+	}
+}
+
+func (ts *FakeTypeTestSuite) initDependencies() {
+	ts.mockCtrl = gomock.NewController(ts.T())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/doctornick42/gosli
 
-require github.com/dave/jennifer v1.2.0
+require (
+	github.com/dave/jennifer v1.2.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/mock v1.2.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.2.2
+)
