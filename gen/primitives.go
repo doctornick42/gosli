7bf3f558de5c6a942448aa608ecd6117bce4bb9a
doctornick <dmitry.bogomolov@iage.net>
2019-01-20 12:43:30 +0300

    Implement GetUnion method for primitives

--- gen/primitives.go
+++ gen/primitives.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/dave/jennifer/jen"
+	. "github.com/dave/jennifer/jen"
 )
 
 const (
@@ -22,7 +22,7 @@ type PrimitivesGenerator struct{}
 func (g *PrimitivesGenerator) Run() error {
 	for _, typeName := range AvailableTypes {
 
-		f := jen.NewFile(primitivesModuleName)
+		f := NewFile(primitivesModuleName)
 		g.generateInfrastructure(f, typeName)
 		generateFirstOrDefault(f, typeName)
 		generateFirst(f, typeName)
@@ -30,6 +30,10 @@ func (g *PrimitivesGenerator) Run() error {
 		generateSelect(f, typeName)
 		generatePage(f, typeName)
 		generateAny(f, typeName)
+		g.generateContains(f, typeName)
+		g.generateGetUnion(f, typeName)
+		// g.generateInFirstOnly(f, "*"+typeName)
+		// g.generateEqualImplementation(f, "*"+typeName)
 
 		fakeOriginPath := fmt.Sprintf("%s/fake.go", primitivesModuleName)
 
@@ -45,7 +49,7 @@ func (g *PrimitivesGenerator) Run() error {
 	return nil
 }
 
-func (g *PrimitivesGenerator) generateInfrastructure(f *jen.File, typeName string) {
+func (g *PrimitivesGenerator) generateInfrastructure(f *File, typeName string) {
 	generateInfrastructure(f, typeName)
 }
 
@@ -53,6 +57,105 @@ func (g *PrimitivesGenerator) getGeneratedFileName(originFilePath, typeName stri
 	return generateFileName(originFilePath, "", typeName)
 }
 
-func (g *PrimitivesGenerator) generateFirst(f *jen.File, typeName string) {
+func (g *PrimitivesGenerator) generateFirst(f *File, typeName string) {
 	generateFirst(f, typeName)
+}
+
+func (g *PrimitivesGenerator) generateContains(f *File, typeName string) {
+	f.Func().
+		Params(
+			Id("r").Id(getStructName(typeName)),
+		).
+		Id("Contains").
+		Params(
+			Id("el").Id(typeName),
+		).
+		Params(
+			Bool(),
+			Error(),
+		).
+		Block(
+			For(
+				Id("_, slEl").Op(":=").Range().Id("r").Block(
+					If(
+						Id("slEl").Op("==").Id("el"),
+					).Block(
+						Return(True(), Nil()),
+					),
+				),
+			),
+
+			Return(False(), Nil()),
+		)
+}
+
+func (g *PrimitivesGenerator) getEqualStatement(el1, el2 string, typeName string) []Code {
+	if string(typeName[0]) == "*" {
+		return []Code{
+			Var().Id("areEqual").Bool(),
+
+			If(
+				Id(el1).Op("==").Nil().Op("&&").
+					Id(el2).Op("==").Nil(),
+			).Block(
+				Id("areEqual").Op("=").True(),
+			),
+
+			If(
+				Params(
+					Id(el1).Op("!=").Nil().Op("&&").
+						Id(el2).Op("==").Nil()).
+					Op("||").
+					Params(
+						Id(el1).Op("==").Nil().Op("&&").
+							Id(el2).Op("!=").Nil(),
+					),
+			).Block(
+				Id("areEqual").Op("=").False(),
+			),
+
+			Id("areEqual").Op("=").Id("*" + el1).Op("==").Id("*" + el2),
+		}
+	}
+
+	return []Code{
+		Id("areEqual").Op(":=").Id(el1).Op("==").Id(el2),
+	}
+}
+
+func (g *PrimitivesGenerator) generateGetUnion(f *File, typeName string) {
+	f.Func().
+		Params(
+			Id("r").Id(getStructName(typeName)),
+		).
+		Id("GetUnion").
+		Params(
+			Id("sl2").Index().Id(typeName),
+		).
+		Params(
+			Index().Id(typeName),
+			Error(),
+		).
+		Block(
+			Id("result").Op(":=").Make(Index().Id(typeName), Lit(0)),
+
+			For(
+				Id("_, sl1El").Op(":=").Range().Id("r").Block(
+					For(
+						Id("_, sl2El").Op(":=").Range().Id("sl2").Block(
+							append(
+								g.getEqualStatement("sl1El", "sl2El", typeName),
+								If(
+									Id("areEqual"),
+								).Block(
+									Id("result").Op("=").Append(Id("result"), Id("sl1El")),
+								),
+							)...,
+						),
+					),
+				),
+			),
+
+			Return(Id("result"), Nil()),
+		)
 }
--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/doctornick42/gosli/gen"
 )
 
 func main() {
-	customGenerator := &gen.CustomGenerator{}
-	err := customGenerator.Run(os.Args[1:])
+	//customGenerator := &gen.CustomGenerator{}
+	//err := customGenerator.Run(os.Args[1:])
 
-	//primitivesGen := &gen.PrimitivesGenerator{}
-	//err := primitivesGen.Run()
+	primitivesGen := &gen.PrimitivesGenerator{}
+	err := primitivesGen.Run()
 
 	if err != nil {
 		log.Fatal(err)
